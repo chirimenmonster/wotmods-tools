5ab8efa7c812e33c9c5f80b8da95e2df6f0e1008
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-03-04 21:54:58 +0900

    refact

--- package.py
+++ package.py
@@ -27,90 +27,102 @@ def makedirs(dir):
         pass
 
 
-def compile_python(src, dst, virtualdir):
-    makedirs(os.path.dirname(dst))
-    vfile = os.path.join(virtualdir, os.path.basename(src))
-    py_compile.compile(file=src, cfile=dst, dfile=vfile, doraise=True)
+class Process(object):
 
+    def __init__(self):
+        self.__feature = {
+            'apply+python': self.__apply_python,
+            'python':       self.__python,
+            'apply':        self.__apply,
+            'plain':        self.__plain
+        }
 
-def apply_template(src, dst, parameters):
-    makedirs(os.path.dirname(dst))
-    with open(src, 'r') as in_file, open(dst, 'w') as out_file:
-        out_file.write(Template(in_file.read()).substitute(parameters))
+    def command(self, cmd, file, reldir, params):
+        return self.__feature[cmd](file, reldir, params)
 
+    def __apply_python(self, file, reldir, params):
+        file = self.__apply(file, reldir, params)
+        file = self.__python(file, reldir, params)
+        return file
 
-def feature_apply(src, params):
-    srcdir, srcfile = os.path.split(src)
-    dstdir = os.path.join(BUILD_DIR, srcdir)
-    name, ext = os.path.splitext(srcfile)
-    if ext == '' or ext == '.in':
-        dst = os.path.join(dstdir, name)
-    else:
-        inname, inext = os.path.splitext(name)
+    def __python(self, file, reldir, params):
+        file = self.feature_compile(file, reldir)
+        return file
+
+    def __apply(self, file, reldir, params):
+        file = self.feature_apply(file, params)
+        return file
+    
+    def __plain(self, file, reldir, params):
+        return file
+
+    def feature_compile(self, src, reldir):
+        srcdir, srcfile = os.path.split(src)
+        dstdir = os.path.join(BUILD_DIR, srcdir)
+        name, ext = os.path.splitext(src)
+        dst = os.path.join(dstdir, name + '.pyc')
+        makedirs(os.path.dirname(dst))
+        vfile = os.path.join(reldir, os.path.basename(src))
+        py_compile.compile(file=src, cfile=dst, dfile=vfile, doraise=True)
+        return dst
+
+    def feature_apply(self, src, params):
+        srcdir, srcfile = os.path.split(src)
+        dstdir = os.path.join(BUILD_DIR, srcdir)
+        name, ext = os.path.splitext(srcfile)
+        if ext == '' or ext == '.in':
+            dst = os.path.join(dstdir, name)
+        else:
+            inname, inext = os.path.splitext(name)
         if inext == '.in':
             dst = os.path.join(dstdir, inname + ext)
         else:
             dst = os.path.join(dstdir, name + ext)
-    apply_template(src, dst, params)
-    return dst
+        makedirs(os.path.dirname(dst))
+        with open(src, 'r') as in_file, open(dst, 'w') as out_file:
+            out_file.write(Template(in_file.read()).substitute(params))
+        return dst
 
 
-def feature_compile(src, reldir):
-    srcdir, srcfile = os.path.split(src)
-    dstdir = os.path.join(BUILD_DIR, srcdir)
-    name, ext = os.path.splitext(src)
-    dst = os.path.join(dstdir, name + '.pyc')
-    compile_python(src, dst, reldir)
-    return dst
+class Package(object):
+
+    def __init__(self):
+        self.__dirs = {}
+        self.__list = []
+
+    def __split(self, path):
+        head, tail = os.path.split(path)
+        if not head:
+            return [ tail ]
+        result = self.__split(head)
+        result.append(path)
+        return result
+    
+    def add(self, src, dst):
+        for dir in self.__split(dst)[0:-1]:
+            if dir not in self.__dirs:
+                self.__dirs[dir] = True
+                self.__list.append([ '.', dir ])
+        self.__list.append([src, dst])
+
+    def createZipfile(self, pkgname, compression):
+        with zipfile.ZipFile(pkgname, 'w', compression=compression) as file:
+            for source, target in self.__list:
+                file.write(source, target, compression)
 
 
-def process_file(method, file, reldir, params):
-    if method == 'apply+python':
-        file = feature_apply(file, params)
-        file = feature_compile(file, reldir)
-    elif method == 'python':
-        file = feature_compile(file, reldir)
-    elif method == 'apply':
-        file = feature_apply(file, params)
-    elif method == 'plain':
-        pass
-    else:
-        raise ValueError('unknown methd: {}'.format(target['method']))
-    return file
-
-
-def split(path):
-    head, tail = os.path.split(path)
-    if not head:
-        return [ tail ]
-    result = split(head)
-    result.append(path)
-    return result
-
-
-def create_zipfile(package, paths, compression=zipfile.ZIP_DEFLATED):
-    donelist = []
-    with zipfile.ZipFile(package, 'w', compression=compression) as file:
-        for source, target in paths:
-            for dir in split(target)[0:-1]:
-                if dir not in donelist:
-                    file.write('.', dir, compression)
-                    donelist.append(dir)
-            file.write(source, target, compression)
-            donelist.append(target)
-
-
-def package(jsonfile, params, compression=zipfile.ZIP_STORED):
+def makePackage(jsonfile, params, compression=zipfile.ZIP_STORED):
     with open(jsonfile, 'r') as f:
-        desc = json.loads(Template(f.read()).substitute(params))   
-    paths = []
+        desc = json.loads(Template(f.read()).substitute(params))
+    package = Package()
+    process = Process()
     for target in desc['files']:
         for file in target['source']:
-            file = process_file(target['method'], file, target['reldir'], params)
+            file = process.command(target['method'], file, target['reldir'], params)
             release = os.path.join(target['root'], target['reldir'], os.path.basename(file))
-            paths.append([ file, release ])
-    package = os.path.join(BUILD_DIR, desc['package'])
-    create_zipfile(package, paths, compression)
+            package.add(file, release)
+    pkgname = os.path.join(BUILD_DIR, desc['package'])
+    package.createZipfile(pkgname, compression)
 
 
 def main():
@@ -121,10 +133,10 @@ def main():
         pass
     os.makedirs(BUILD_DIR)
 
-    package(params['wotmod_files'], params)
+    makePackage(params['wotmod_files'], params)
 
     if 'release_files' in params and os.path.exists(params['release_files']):
-        package(params['release_files'], params, compression=zipfile.ZIP_DEFLATED)
+        makePackage(params['release_files'], params, compression=zipfile.ZIP_DEFLATED)
 
 
 if __name__ == "__main__":
