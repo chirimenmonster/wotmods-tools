aa757ab7b895bac61b07cd28f288773902733210
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-03-06 19:46:33 +0900

    refact and modularize

--- package.py
+++ package.py
@@ -7,18 +7,21 @@ import ConfigParser
 import json
 from string import Template
 
-BUILD_DIR = 'build'
 CONFIG = 'config.ini'
+DEFAULT_BUILDDIR = 'build'
 
-def get_config():
-    inifile = ConfigParser.SafeConfigParser()
-    inifile.read(CONFIG)
+SECTION_WOTMOD = 'wotmod'
+SECTION_RELEASE = 'release'
 
-    parameters = dict(inifile.items('wotmod'))
-    for section in inifile.sections():
-        for k, v in inifile.items(section):
-            parameters[section + '_' + k] = v
-    return parameters
+PKGDEF = {
+    SECTION_WOTMOD:     SECTION_WOTMOD + '_pkgdef',
+    SECTION_RELEASE:    SECTION_RELEASE + '_pkgdef'
+}
+
+PKGNAME = {
+    SECTION_WOTMOD:     SECTION_WOTMOD + '_package',
+    SECTION_RELEASE:    SECTION_RELEASE + '_package'
+}
 
 
 def makedirs(dir):
@@ -27,10 +30,114 @@ def makedirs(dir):
     except:
         pass
 
+def splitpath(path):
+    head, tail = os.path.split(path)
+    if not head:
+        return [ tail ]
+    return splitpath(head) + [ tail ]
+
+    
+class Control(object):
+
+    def __init__(self, config=CONFIG):
+        self.setConfig(config)
+        self.__buildDir = self.__params.get('build_dir', DEFAULT_BUILDDIR)
+        self.__params['BUILDDIR'] = self.__buildDir
+        self.__process = Process(self.__buildDir)
+        self.makeBuildDir(self.__buildDir)
+
+    def makeBuildDir(self, buildDir):
+        try:
+            shutil.rmtree(buildDir)
+        except:
+            pass
+        os.makedirs(buildDir)
+
+    def setConfig(self, config=CONFIG):
+        inifile = ConfigParser.SafeConfigParser()
+        inifile.read(config)
+        params = dict(inifile.items(SECTION_WOTMOD))
+        for section in inifile.sections():
+            for k, v in inifile.items(section):
+                params[section + '_' + k] = v
+        self.__params = params
+        return params
+
+    def getParam(self, key):
+        return self.__params[key]
+
+    def setPackageDef(self, filename):
+        with open(filename, 'r') as file:
+            text = Template(file.read()).substitute(self.__params)
+            packageDef = json.loads(text)
+        self.__packageDef = packageDef
+        return packageDef
+
+    def makePackage(self, domain=SECTION_WOTMOD, compression=zipfile.ZIP_STORED):
+        pkgsrcs = self.__params[PKGDEF[domain]]
+        pkgname = os.path.join(self.__buildDir, self.__params[PKGNAME[domain]])
+        packageDef = PackageDef(pkgsrcs, self.__params)
+        package = Package()
+        for recipe in packageDef.getRecipes():
+            file = self.__process.command(recipe)
+            release = os.path.join(recipe.root, recipe.reldir, os.path.basename(file))
+            package.add(file, release)
+            # print '{}: {} -> {}'.format(recipe.method, recipe.file, file)
+        package.createZipfile(pkgname, compression)
+        return pkgname
+
+
+class PackageDef(object):
+
+    def __init__(self, filename, params):
+        self.__params = params
+        with open(filename, 'r') as file:
+            text = Template(file.read()).substitute(self.__params)
+            packageDef = json.loads(text)
+        self.__packageDef = packageDef
+
+    def get(self, key):
+        return self.__params[key]
+
+    def getRecipes(self):
+        self.__recipes = []
+        for desc in self.__packageDef['sources']:
+            recipes = self.parse(desc)
+            self.__recipes.extend(recipes)
+        return self.__recipes
+
+    def parse(self, desc):
+        recipes = []
+        for file in desc.get('files', []):
+            recipes.append(self.__parseSource(desc, file))
+        for dir in desc.get('dirs', []):
+            recipes.extend(self.__parseSrcDir(desc, dir))
+        return recipes
+
+    def __parseSource(self, desc, srcfile):
+        recipe = Recipe(srcfile, desc.get('reldir', ''), desc, self.__params)
+        return recipe
+
+    def __parseSrcDir(self, desc, srcdir):
+        recipes = []
+        for root, dirs, files in os.walk(srcdir):
+            relpath = os.path.relpath(root, srcdir)
+            if relpath == '.':
+                relpath = ''
+            reldir = os.path.join(desc.get('reldir', ''), relpath)
+            for file in sorted(files):
+                if 'include' in desc and not re.search(desc['include'], file):
+                    continue
+                if 'exclude' in desc and re.search(desc['exclude'], file):
+                    continue
+                recipes.append(Recipe(os.path.join(root, file), reldir, desc, self.__params))
+        return recipes
+ 
 
 class Process(object):
 
-    def __init__(self):
+    def __init__(self, buildDir):
+        self.__buildDir = buildDir
         self.__feature = {
             'python':       self.__feature_compile,
             'apply':        self.__feature_apply
@@ -53,25 +160,26 @@ class Process(object):
                 file = self.__getFilename[m](file)
         return file
 
-
     def __getFilename_compile(self, src):
-        srcdir, srcfile = os.path.split(src)
-        dstdir = os.path.join(BUILD_DIR, srcdir)
-        name, ext = os.path.splitext(srcfile)
-        return os.path.join(dstdir, name + '.pyc')
+        dir, file = os.path.split(src)
+        if splitpath(dir)[0] != self.__buildDir:
+            dir = os.path.join(self.__buildDir, dir)
+        name, ext = os.path.splitext(file)
+        return os.path.join(dir, name + '.pyc')
 
     def __getFilename_apply(self, src):
-        srcdir, srcfile = os.path.split(src)
-        dstdir = os.path.join(BUILD_DIR, srcdir)
-        name, ext = os.path.splitext(srcfile)
+        dir, file = os.path.split(src)
+        if splitpath(dir)[0] != self.__buildDir:
+            dir = os.path.join(self.__buildDir, dir)
+        name, ext = os.path.splitext(file)
         if ext == '' or ext == '.in':
-            dst = os.path.join(dstdir, name)
+            dst = os.path.join(dir, name)
         else:
             inname, inext = os.path.splitext(name)
             if inext == '.in':
-                dst = os.path.join(dstdir, inname + ext)
+                dst = os.path.join(dir, inname + ext)
             else:
-                dst = os.path.join(dstdir, name + ext)
+                dst = os.path.join(dir, name + ext)
         return dst
 
     def __feature_compile(self, src, recipe):
@@ -95,19 +203,13 @@ class Package(object):
         self.__dirs = {}
         self.__list = []
 
-    def __split(self, path):
-        head, tail = os.path.split(path)
-        if not head:
-            return [ tail ]
-        result = self.__split(head)
-        result.append(path)
-        return result
-    
     def add(self, src, dst):
-        for dir in self.__split(dst)[0:-1]:
-            if dir not in self.__dirs:
-                self.__dirs[dir] = True
-                self.__list.append([ '.', dir ])
+        path = ''
+        for dir in splitpath(dst)[0:-1]:
+            path = os.path.join(path, dir)
+            if path not in self.__dirs:
+                self.__dirs[path] = True
+                self.__list.append([ '.', path ])
         self.__list.append([src, dst])
 
     def list(self):
@@ -119,7 +221,7 @@ class Package(object):
             for source, target in self.__list:
                 file.write(source, target, compression)
 
-                
+
 class Recipe(object):
 
     def __init__(self, file, reldir, target, params):
@@ -128,62 +230,12 @@ class Recipe(object):
         self.method = target['method']
         self.root = target.get('root', '')
         self.params = params
-        self.cooked = Process().getFilename(self.method, self.file) 
  
 
-def parsePackage(target, params):
-    list = []
-    for file in target.get('source', []):
-        desc = Recipe(file, target.get('reldir', ''), target, params)
-        list.append(desc) 
-    for dir in target.get('srcdir', []):
-        for root, dirs, files in os.walk(dir):
-            relpath = os.path.relpath(root, dir)
-            if relpath == '.':
-                relpath = ''
-            reldir = os.path.join(target.get('reldir', ''), relpath)
-            for file in sorted(files):
-                if 'include' in target and not re.search(target['include'], file):
-                    continue
-                if 'exclude' in target and re.search(target['exclude'], file):
-                    continue
-                desc = Recipe(os.path.join(root, file), reldir, target, params)
-                list.append(desc) 
-    return list
-
-
-def makePackage(jsonfile, params, compression=zipfile.ZIP_STORED):
-    with open(jsonfile, 'r') as f:
-        desc = json.loads(Template(f.read()).substitute(params))
-    package = Package()
-    list = []
-    for target in desc['files']:
-        list.extend(parsePackage(target, params))
-    process = Process()
-    for recipe in list:
-        file = process.command(recipe)
-        release = os.path.join(recipe.root, recipe.reldir, os.path.basename(file))
-        package.add(file, release)
-        # print '{}: {} -> {}'.format(recipe.method, recipe.file, recipe.cooked)
-    pkgname = os.path.join(BUILD_DIR, desc['package'])
-    package.createZipfile(pkgname, compression)       
-    return pkgname
-
-
 def main():
-    params = get_config()
-    try:
-        shutil.rmtree(BUILD_DIR)
-    except:
-        pass
-    os.makedirs(BUILD_DIR)
-
-    file = makePackage(params['wotmod_files'], params)
+    control = Control()
+    file = control.makePackage()
     print 'create package: {}'.format(file)
-
-    if 'release_files' in params and os.path.exists(params['release_files']):
-        file = makePackage(params['release_files'], params, compression=zipfile.ZIP_DEFLATED)
-        print 'create package: {}'.format(file)
 
 
 if __name__ == "__main__":
