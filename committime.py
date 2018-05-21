33825a9e570bd13d962b0e28edab72c5f511c8bd
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-05-21 19:02:31 +0900

    detect modified files

--- committime.py
+++ committime.py
@@ -11,14 +11,21 @@ def getFileList():
 def getGitCommitLogs():
     return commands.getoutput('git log -m -r --name-only --no-color --pretty=raw -z').split('\0\0')
 
+def getChangeFiles():
+    result = commands.getoutput('git diff --ignore-cr-at-eol --numstat').split('\n')
+    result = filter(lambda s: not re.match(r'0\t0\t.*$', s), result)
+    result = map(lambda s: s.split('\t')[2], result)
+    return result
 
 class CommitTime(object):
 
     def __init__(self):
         filelist = getFileList()
         logs = getGitCommitLogs()
+        changefiles = getChangeFiles()
         self.__timestamp = {}
         self.setCommitTime(logs, filelist)
+        self.setChangeTime(changefiles)
         self.lastupdate = max(self.__timestamp.values())
 
     def __getCommitTime(self, log):
@@ -42,6 +49,18 @@ class CommitTime(object):
                     if len(filelist) == 0:
                         return
 
+    def setChangeTime(self, files):
+        for f in files:
+            mtime = int(os.stat(f).st_mtime)
+            self.__timestamp[f] = mtime
+ 
     def getTimestamp(self, file):
-        return self.__timestamp.get(file, None)
+        return self.__timestamp.get(file, int(os.stat(file).st_mtime))
 
+    def dump(self):
+        for k, v in self.__timestamp.items():
+            print '{} {}'.format(datetime.fromtimestamp(v), k)
+
+if __name__ == "__main__":
+    ct = CommitTime()
+    ct.dump()
--- package.py
+++ package.py
@@ -56,61 +56,60 @@ class Config(object):
     def getDict(self):
         return self.__params
 
+    @property
+    def dict(self):
+        return self.__params
+
+    @property
+    def buildDir(self):
+        return self.__params.get('build_dir', DEFAULT_BUILDDIR)
+
 
 class Control(object):
 
     def __init__(self, config=CONFIG):
-        self.__lastupdate = 0
         self.__commitTime = committime.CommitTime()
-        if isinstance(config, list):
-            self.__configTimestamp = max([ self.__commitTime.getTimestamp(f) for f in config ])
-        else:
-            self.__configTimestamp = self.__commitTime.getTimestamp(config)
         self.setConfig(config)
         self.makeBuildDir()
     
     def setConfig(self, config=CONFIG):
         self.__params = Config(config)
-        self.__buildDir = self.__params.get('build_dir', DEFAULT_BUILDDIR)
+        if isinstance(config, list):
+            timestamp = max([ self.__commitTime.getTimestamp(f) for f in config ])
+        else:
+            timestamp = self.__commitTime.getTimestamp(config)
+        self.__params.timestamp = timestamp
 
     def makeBuildDir(self):
         try:
-            shutil.rmtree(self.__buildDir)
+            shutil.rmtree(self.__params.buildDir)
         except:
             pass
-        os.makedirs(self.__buildDir)
+        os.makedirs(self.__params.buildDir)
 
     def makePackage(self, domain=SECTION_WOTMOD, compression=zipfile.ZIP_STORED):
         pkgsrcs = self.__params.get(PKGDEF[domain])
-        pkgname = os.path.join(self.__buildDir, self.__params.get(PKGNAME[domain]))
-        packageDef = PackageDef(pkgsrcs, self.__params.getDict(), self.__commitTime, self.__configTimestamp, self.__lastupdate)
-        self.__lastupdate = packageDef.getLastupdate()
-        packageDef.dump()
-        print 'Lastupdate: {}'.format(datetime.fromtimestamp(self.__lastupdate))
-        package = Package()
-        process = Process(self.__buildDir)
-        for recipe in packageDef.getRecipes():
-            package.add(*process.command(recipe))
-        #package.list()
-        package.setLastupdate(packageDef.getLastupdate())
-        package.createZipfile(pkgname, compression)
+        pkgname = os.path.join(self.__params.buildDir, self.__params.get(PKGNAME[domain]))
+        resources = Resources(pkgsrcs, self.__params, self.__commitTime)
+        #resources.dump()
+        zipPackage = ZipPackage()
+        for recipe in resources.getRecipes():
+            file, vpath, timestamp = Process().command(recipe)
+            zipPackage.add(file, vpath, timestamp)
+        zipPackage.create(pkgname, compression)
         return pkgname
 
 
-class PackageDef(object):
+class Resources(object):
 
-    def __init__(self, filename, params, commitTime, configTimestamp, lastupdate):
+    def __init__(self, filename, params, commitTime):
         self.__commitTime = commitTime
-        self.__configTimestamp = configTimestamp
-        self.__lastupdate = lastupdate
         self.__params = params
-        self.__process = Process(self.__params.get('build_dir', DEFAULT_BUILDDIR))
         with open(filename, 'r') as file:
-            text = Template(file.read()).substitute(self.__params)
-            packageDef = json.loads(text)
-        self.__packageDef = packageDef
+            text = Template(file.read()).substitute(self.__params.dict)
+        self.__resource = json.loads(text)
         self.__recipes = []
-        for desc in self.__packageDef['sources']:
+        for desc in self.__resource['sources']:
             recipes = self.parse(desc)
             self.__recipes.extend(recipes)
 
@@ -120,9 +119,6 @@ class PackageDef(object):
     def getRecipes(self):
         return self.__recipes
 
-    def getLastupdate(self):
-        return max([ r.timestamp for r in self.__recipes ])
-
     def dump(self):
         for recipe in self.__recipes:
             print '{}: {}: {} -> {}'.format(datetime.fromtimestamp(recipe.timestamp), recipe.method,
@@ -131,7 +127,8 @@ class PackageDef(object):
     def parse(self, desc):
         recipes = []
         for file in desc.get('files', []):
-            recipes.append(self.__getRecipe(file, desc.get('reldir', ''), desc))
+            reldir = desc.get('reldir', '')
+            recipes.append(self.__getRecipe(file, reldir, desc))
         for dir in desc.get('dirs', []):
             recipes.extend(self.__parseSrcDir(desc, dir))
         return recipes
@@ -148,25 +145,20 @@ class PackageDef(object):
                     continue
                 if 'exclude' in desc and re.search(desc['exclude'], file):
                     continue
-                recipes.append(self.__getRecipe(os.path.join(root, file), reldir, desc))
+                src = os.path.join(root, file)
+                recipes.append(self.__getRecipe(src, reldir, desc))
         return recipes
 
     def __getRecipe(self, src, reldir, desc):
-        timestamp = self.__commitTime.getTimestamp(src) or self.__lastupdate
-        if 'apply' in desc['method']:
-            timestamp = max(timestamp, self.__configTimestamp)
-        dst = self.__process.getFilename(desc['method'], src)
-        vpath = os.path.join(desc.get('root', ''), reldir, os.path.basename(dst))
-        recipe = Recipe(src, reldir, desc, self.__params, timestamp)
-        recipe.dst = dst
-        recipe.vpath = vpath
+        #timestamp = self.__commitTime.getTimestamp(src) or self.__lastupdate
+        timestamp = self.__commitTime.getTimestamp(src)
+        recipe = Recipe(src, reldir, desc, timestamp, self.__params)
         return recipe
 
 
 class Process(object):
 
-    def __init__(self, buildDir):
-        self.__buildDir = buildDir
+    def __init__(self):
         self.__feature = {
             'python':       self.__feature_compile,
             'apply':        self.__feature_apply
@@ -178,41 +170,50 @@ class Process(object):
 
     def command(self, recipe):
         file = recipe.file
-        for method in recipe.method.split('+'):
-            if method != 'plain':
-                file = self.__feature[method](file, recipe)
+        for p in recipe.process:
+            file = self.__feature[p.method](p.src, p.dst, recipe)
         return file, recipe.vpath, recipe.timestamp
 
-    def getFilename(self, method, file):
+    def getFilename(self, method, file, buildDir):
+        process = []
         for m in method.split('+'):
             if m != 'plain':
-                file = self.__getFilename[m](file)
-        return file
+                newfile = self.__getFilename[m](file, buildDir)
+                process.append(ProcessDesc(m, file, newfile))
+                file = newfile
+        return process
 
-    def __getFilename_compile(self, src):
+    def getTimestamp(self, method, timestamp, configTimestamp):
+        if 'apply' in method:
+            timestamp = max(timestamp, configTimestamp)
+        return timestamp
+
+    def __getDstdir(self, dir, buildDir):
+        if splitpath(dir)[0] != buildDir:
+            dir = os.path.join(buildDir, dir)
+        return dir
+
+    def __getFilename_compile(self, src, buildDir):
         dir, file = os.path.split(src)
-        if splitpath(dir)[0] != self.__buildDir:
-            dir = os.path.join(self.__buildDir, dir)
+        dstdir = self.__getDstdir(dir, buildDir)
         name, ext = os.path.splitext(file)
-        return os.path.join(dir, name + '.pyc')
+        return os.path.join(dstdir, name + '.pyc')
 
-    def __getFilename_apply(self, src):
+    def __getFilename_apply(self, src, buildDir):
         dir, file = os.path.split(src)
-        if splitpath(dir)[0] != self.__buildDir:
-            dir = os.path.join(self.__buildDir, dir)
+        dstdir = self.__getDstdir(dir, buildDir)
         name, ext = os.path.splitext(file)
         if ext == '' or ext == '.in':
-            dst = os.path.join(dir, name)
+            dst = os.path.join(dstdir, name)
         else:
             inname, inext = os.path.splitext(name)
             if inext == '.in':
-                dst = os.path.join(dir, inname + ext)
+                dst = os.path.join(dstdir, inname + ext)
             else:
-                dst = os.path.join(dir, name + ext)
+                dst = os.path.join(dstdir, name + ext)
         return dst
 
-    def __feature_compile(self, src, recipe):
-        dst = self.__getFilename_compile(src)
+    def __feature_compile(self, src, dst, recipe):
         makedirs(os.path.dirname(dst))
         vfile = os.path.join(recipe.reldir, os.path.basename(src))
         tmpfile = os.path.join(os.path.dirname(dst), os.path.basename(src))
@@ -222,23 +223,18 @@ class Process(object):
         py_compile.compile(file=tmpfile, cfile=dst, dfile=vfile, doraise=True)
         return dst
 
-    def __feature_apply(self, src, recipe):
-        dst = self.__getFilename_apply(src)
+    def __feature_apply(self, src, dst, recipe):
         makedirs(os.path.dirname(dst))
         with open(src, 'r') as in_file, open(dst, 'w') as out_file:
-            out_file.write(Template(in_file.read()).substitute(recipe.params))
+            out_file.write(Template(in_file.read()).substitute(recipe.params.dict))
         return dst
 
 
-class Package(object):
+class ZipPackage(object):
 
     def __init__(self):
         self.__dirs = {}
         self.__list = []
-        self.__lastupdate = None
-
-    def setLastupdate(self, lastupdate):
-        self.__lastupdate = lastupdate
 
     def add(self, src, dst, timestamp):
         path = ''
@@ -246,35 +242,64 @@ class Package(object):
             path = os.path.join(path, dir)
             if path not in self.__dirs:
                 self.__dirs[path] = True
-                self.__list.append([ None, path + '/', None ])
+                self.__list.append([ None, path + '/', timestamp ])
+            else:
+                for i, d in enumerate(self.__list):
+                    if path + '/' == d[1]:
+                        self.__list[i][2] = max(d[2], timestamp)
         self.__list.append([src, dst, timestamp])
 
     def list(self):
         for source, target, timestamp in self.__list:
             print source, target, timestamp
 
-    def createZipfile(self, pkgname, compression):
+    def create(self, pkgname, compression):
         with zipfile.ZipFile(pkgname, 'w', compression=compression) as file:
             for source, target, timestamp in self.__list:
-                t = datetime.fromtimestamp(timestamp or self.__lastupdate)
+                t = datetime.fromtimestamp(timestamp)
                 zipinfo = zipfile.ZipInfo(target, t.timetuple())
                 if source:
                     with open(source, 'rb') as f:
                         file.writestr(zipinfo, f.read(), compression)
-                    #file.write(source, target, compression)
                 else:
                     file.writestr(zipinfo, '', zipfile.ZIP_STORED)
 
 
 class Recipe(object):
 
-    def __init__(self, file, reldir, target, params, timestamp):
+    def __init__(self, file, reldir, target, timestamp, params):
         self.file = file
         self.reldir = reldir
-        self.method = target['method']
-        self.root = target.get('root', '')
+        self.target = target
         self.params = params
-        self.timestamp = timestamp
+        self.process = Process().getFilename(self.method, file, params.buildDir)
+        self.timestamp = Process().getTimestamp(self.method, timestamp, params.timestamp)
+ 
+    @property
+    def method(self):
+        return self.target['method']
+
+    @property
+    def root(self):
+        return self.target.get('root', '')
+
+    @property
+    def vpath(self):
+        return os.path.join(self.root, self.reldir, os.path.basename(self.dst))
+
+    @property
+    def dst(self):
+        if self.process:
+            return self.process[-1].dst
+        return self.file
+
+
+class ProcessDesc(object):
+
+    def __init__(self, method, src, dst):
+        self.method = method
+        self.src = src
+        self.dst = dst
 
 
 def main():
