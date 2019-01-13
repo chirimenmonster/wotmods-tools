66d89542b9d7c4d69ffda6808e9edbd4d6f34901
chirimenmonster <chirimenmonster@users.noreply.github.com>
2019-01-13 23:44:47 +0900

    wip

--- test.py
+++ test.py
@@ -7,18 +7,18 @@ from utils import XmlUnpacker
 from utils import wotmod
 
 
+ORG_PKG = '/c/games/World_of_Tanks_EU/res/packages/scripts.pkg'
+PATTERN = '.*/item_defs/vehicles/.*\.xml'
+WD_EXTRACT = 'tmp/orig'
+WD_MODIFIED = 'tmp/dst'
+
 if __name__ == '__main__':
     print 'extract fropm pkg'
-    unzip.extractPattern('/c/games/World_of_Tanks_EU/res/packages/scripts.pkg', extract_dir='tmp/orig', pattern='.*/item_defs/vehicles/.*\.xml')
+    unzip.extractPattern(ORG_PKG, extract_dir=WD_EXTRACT, pattern=PATTERN)
 
     print 'modify XML files'
-    fileopt = {
-        'base_dir':      'tmp/orig',
-        'extract_dir':   'tmp/dst',
-        'files':        [ 'scripts' ]
-    }
-    for file in utils.path.getFileList(fileopt['files'], base_dir=fileopt['base_dir']):
-        path = os.path.join(fileopt['base_dir'], file)
+    for file in utils.path.getFileList([ 'scripts' ], base_dir=WD_EXTRACT):
+        path = os.path.join(WD_EXTRACT, file)
         root = XmlUnpacker.getElementTree(path, True)
         elemlist = root.findall('.//gunCamPosition/..')
         if not len(elemlist):
@@ -26,9 +26,9 @@ if __name__ == '__main__':
         for elem in elemlist:
             for e in elem.iterfind('gunCamPosition'):
                 elem.remove(e)
-        dst = os.path.join(fileopt['extract_dir'], file)
+        dst = os.path.join(WD_MODIFIED, file)
         XmlUnpacker.outputElementTree(root, dst)
 
     print 'create wotmod package'
-    wotmod.createSimplePackage('test.wotmod', base_dir='tmp/dst', dest_dir='res')
+    wotmod.createSimplePackage('test.wotmod', base_dir=WD_MODIFIED, dest_dir='res')
  
