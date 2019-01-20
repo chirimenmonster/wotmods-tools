cc0ba89ca7d570d046ac14bad833d8b3100232de
chirimenmonster <chirimenmonster@users.noreply.github.com>
2019-01-20 19:20:37 +0900

    integration guessing filepath

--- utils/wot.py
+++ utils/wot.py
@@ -72,7 +72,6 @@ def getWotVersion(base_dir=None):
     return version
 
 
-
 def guessFilePath(name, base_dir=None):
     if os.path.isfile(name):
         return name
@@ -92,28 +91,12 @@ def guessFilePath(name, base_dir=None):
 
 def fetchXmlData(file, base_dir=None, package=None):
     if package:
-        _, ext = os.path.splitext(package)
-        if ext == '.wotmod':
-            target = package
-        elif ext == '.pkg':
-            if os.path.isfile(package):
-                tatrget = package
-            else:
-                target = os.path.join(base_dir, 'res/packages', package)
-        else:
-            target = package
-        zip = unzip.ZipPackage(target, mode='r')
+        path = guessFilePath(package, base_dir=base_dir)
+        zip = unzip.ZipPackage(path, mode='r')
         data = zip.read(file)
     else:
-        if os.path.isfile(file):
-            target = file
-        elif os.path.isfile(os.path.join(base_dir, file)):
-            target = os.path.join(base_dir, file)
-        elif os.path.isfile(os.path.join(base_dir, 'res', file)):
-            target = os.path.join(base_dir, 'res', file)
-        else:
-            target = file
-        with open(file, 'rb') as fp:
+        path = guessFilePath(file, base_dir=base_dir)
+        with open(path, 'rb') as fp:
             data = fp.read() 
     xmlunpacker = XmlUnpacker.XmlUnpacker()
     tree = xmlunpacker.read(io.BytesIO(data), os.path.basename(file))
@@ -122,9 +105,3 @@ def fetchXmlData(file, base_dir=None, package=None):
     else:
         text = data
     return { 'data': tree, 'text': text }
-
-
-def listPackageFile(package, base_dir=None):
-    zip = unzip.ZipPackage(os.path.join(base_dir, 'res/packages', package), mode='r')
-    return zip.namelist()
-
